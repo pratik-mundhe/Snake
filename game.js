29a7ae68d116db1a566f95e2cd7cfa6aa9fc2d62
Pratik Mundhe <61745194+pratik-mundhe@users.noreply.github.com>
2022-02-25 19:47:17 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,52 @@
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection } from './snake.js'
+import { update as updateFood, draw as drawFood } from './food.js'
+import { outsideGrid } from './grid.js'
+
+let lastRenderTime = 0
+let gameOver = false
+const gameBoard = document.getElementById('game-board')
+const gameOverMessage = document.getElementById('game-over-message');
+
+function main(currentTime) {
+  if (gameOver) {
+    gameOverMessage.style.opacity = 1;
+
+    setTimeout(() => {
+      gameOverMessage.style.opacity = 0;
+      setTimeout(() => {
+        window.location.reload();
+      }, 1000);
+    }, 3000)
+
+    return
+  }
+
+
+  window.requestAnimationFrame(main)
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
+
+
+  lastRenderTime = currentTime
+
+  update()
+  draw()
+}
+
+window.requestAnimationFrame(main)
+
+function update() {
+  updateSnake()
+  updateFood()
+  checkDeath()
+}
+
+function draw() {
+  gameBoard.innerHTML = ''
+  drawSnake(gameBoard)
+  drawFood(gameBoard)
+}
+
+function checkDeath() {
+  gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
+}
